f97d8fbb0919de2ef361f50842c4de7fb99d7705
bmoise1 <105768091+bmoise1@users.noreply.github.com>
2023-02-09 00:15:29 -0800

    Add files via upload

---
+++ Test_1 notebook.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Jupyter Notebook Exercices
+
+# Creating a Jupyter notebook and sharing it on github.
+
+# My first tested code in Jupyter:
+
+# In[11]:
+
+
+def multiply(x, y):
+    """Multiply two numbers and return the result."""
+    result = x * y
+    return result
+
+result = multiply(200, 102)
+
+print(result)
+
+
+# In[ ]:
+
+
+
+
